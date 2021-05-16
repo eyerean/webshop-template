b9f16c02fa11c939e7bf674485de42a6293d4561
eyerean <eptinea@gmail.com>
2021-05-16 15:02:00 +0100

    update server for heroku deployment

--- server.js
+++ server.js
@@ -1,20 +1,28 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-}
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
+// API calls
 app.get('/api', (req, res) => {
   res.send({
     message: 'The Express backend is connected to React!',
   });
 });
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
+if (process.env.NODE_ENV === 'production') {
+  // serve static files
+  app.use(express.static('client/build'));
+
+  // handle React routing, return all requests to the React app
+  app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
